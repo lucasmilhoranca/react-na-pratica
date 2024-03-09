fbf38c03c7c41c28156a4a86e950c7e93d73be84
lucasmilhoranca <lucas1234milhoranca@gmail.com>
2024-03-08 23:53:11 -0300

    first commit

---
+++ src/hooks/use-debounce-value.ts
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+export default function useDebounceValue<T= unknown>(value: T, delay: number) {
+    const [deebouncedValue, setDebouncedValue] = useState(value);
+
+    useEffect(() => {
+        const handler = setTimeout(() => {
+            setDebouncedValue(value)
+        }, delay)
+
+        return () => {
+            clearTimeout(handler)
+        }
+    }, [value, delay])
+    return deebouncedValue
+}
